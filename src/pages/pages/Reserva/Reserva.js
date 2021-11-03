5fb6b7d5ce9d39b95a16faa238cf9a1aa5174cce
jesus <jesusromanm99@fpuna.edu.py>
2021-11-02 23:11:59 -0300

    se agrega pantalla de editar reserva

--- src/pages/pages/Reserva/Reserva.js
+++ src/pages/pages/Reserva/Reserva.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Text,StyleSheet } from "react-native";
 import { DataTable } from 'react-native-paper';
-
+import { Ionicons,AntDesign } from '@expo/vector-icons';
+import colors from "../../res/colors";
 import { FAB } from 'react-native-paper';
 
 
@@ -10,34 +11,32 @@ export default function Reserva({navigation}){
    
     const goToCreateReservation=()=>{
             navigation.navigate('CrearReserva')
+    }
+    const goToEditReservation=()=>{
+        navigation.navigate('EditarReserva')
     }   
     return(
         <>
-             <DataTable>
+             <DataTable style={{marginLeft:0}}>
                 <DataTable.Header>
-                    <DataTable.Title>Id</DataTable.Title>
-                    <DataTable.Title>Empleado</DataTable.Title>
-                    <DataTable.Title>Cliente</DataTable.Title>
-                    <DataTable.Title>Desde</DataTable.Title>
-                    <DataTable.Title>Hasta</DataTable.Title>
+                    
+                    <DataTable.Title style={styles.cell}>Empleado</DataTable.Title>
+                    <DataTable.Title style={styles.cell}>Cliente</DataTable.Title>
+                    <DataTable.Title style={styles.cell}>Desde</DataTable.Title>
+                    <DataTable.Title style={styles.cell}>Hasta</DataTable.Title>
+                    <DataTable.Title numeric>Editar</DataTable.Title>
 
                 </DataTable.Header>
 
-                <DataTable.Row>
-                    <DataTable.Cell>1</DataTable.Cell>
-                    <DataTable.Cell >Jesus</DataTable.Cell>
-                    <DataTable.Cell >Carlos</DataTable.Cell>
-                    <DataTable.Cell >10/11/2021</DataTable.Cell>
-                    <DataTable.Cell >12/02/2000</DataTable.Cell>
+                <DataTable.Row>  
+                    <DataTable.Cell  style={styles.cell}>Jesus</DataTable.Cell>
+                    <DataTable.Cell style={styles.cell} >Carlos</DataTable.Cell>
+                    <DataTable.Cell  style={styles.cell}>10/11/2021</DataTable.Cell>
+                    <DataTable.Cell  style={styles.cell}>12/02/2000</DataTable.Cell>
+                    <DataTable.Cell numeric onPress={goToEditReservation}><AntDesign  name="edit" size={20} color={colors.primary}/></DataTable.Cell>
                 </DataTable.Row>
 
-                <DataTable.Row>
-                    <DataTable.Cell>1</DataTable.Cell>
-                    <DataTable.Cell >Pepe</DataTable.Cell>
-                    <DataTable.Cell >Enrique</DataTable.Cell>
-                    <DataTable.Cell >10/11/2021</DataTable.Cell>
-                    <DataTable.Cell >12/02/2000</DataTable.Cell>
-                </DataTable.Row>
+                
              </DataTable>     
             
             <FAB
@@ -61,5 +60,10 @@ const styles = StyleSheet.create({
         margin: 16,
         right: 0,
         bottom: 0,
+    },
+    cell:{
+        
+        flex:2,
+      
     }
   });
