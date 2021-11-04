e2803df0d512d5f01665ce71a79af3555a3c6f6b
jesus <jesusromanm99@fpuna.edu.py>
2021-11-04 08:36:25 -0300

    Se agrega los campos en crear y Editar Reserva

--- src/pages/pages/Reserva/CrearReserva.js
+++ src/pages/pages/Reserva/CrearReserva.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { Button, Text } from "react-native-paper";
+import { Button, Text,TextInput,Divider } from "react-native-paper";
 import { createReservation,getScheduleClear } from "../../libs/http";
+import DropDown from "react-native-paper-dropdown";
+import { StyleSheet,View } from "react-native";
+import colors from "../../res/colors";
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 const CrearReserva=()=>{
+    /* States */
     const [name,setName]=React.useState('Reserva')
+    const [idEmpleado,setEmpleado]=React.useState('')
+    const [idCliente,setIdCliente]=React.useState('')
+    const [fechaCadena,setFechaCadena]=React.useState("20190903")
+    const [fecha,setFecha]=React.useState(new Date())
+    const [horaInicioCadena,setHoraInicioCadena]=React.useState('1000')
+    const [horaFinCadena,setHoraFinCadena]=React.useState('1015')
+    const [showDropDownFisio, setShowDropDownFisio] = React.useState(false);
+    const [showDropDownClient, setShowDropDownClient] = React.useState(false);
+    const [showDate,setShowDate]=React.useState(false)
+
+    /*   variables temporables */
     const addReservation=async()=>{
             const {data,error}=await createReservation(
                 {
@@ -14,15 +31,100 @@ const CrearReserva=()=>{
                 }
             )
     }
+    const FisioOptions=[
+        {
+            'label':'Jesus',
+            'value':'1'
+        },
+        {
+            'label':'Carlos',
+            'value':'2'
+        }
+    ]
+
+    const ClientOptions=[
+        {
+            'label':'Juan',
+            'value':'1'
+        },
+        {
+            'label':'Fer',
+            'value':'2'
+        }
+    ]
+
+    /* Methods */
     const getScheduleFree=async()=>{
         const {data,error}=await getScheduleClear(4,'20190903')
     }
+    const onChangeDate=(event, selectedDate) => {
+        const currentDate = selectedDate || fecha;
+        setShowDate(false);
+        setFecha(currentDate);
+      };
+    /* Use Effect */
     return(
-        <>
-            <Text> Crear una {name}</Text>
-            <Button onPress={addReservation}>Crear Reserva</Button>
-            <Button onPress={getScheduleFree}>Obtener agenda Libre</Button>
-        </>
+        <View style={styles.container}> 
+            <Text style={styles.containeritem}> Filtro:</Text>
+            <DropDown
+                     label={"Fisioterapeuta"}
+                     mode={"outlined"}
+                     visible={showDropDownFisio}
+                     showDropDown={() => setShowDropDownFisio(true)}
+                     onDismiss={() => setShowDropDownFisio(false)}
+                     value={idEmpleado}
+                     setValue={setEmpleado}
+                     list={FisioOptions}
+                     style={styles.containeritem}
+            />
+            <Button
+                labelStyle={{textDecorationLine:'underline'}}
+                uppercase={false}
+                onPress={()=>setShowDate(true)}
+            >  Seleccionar una fecha</Button>
+           
+            {showDate && 
+                <DateTimePicker
+                testID="dateTimePicker"
+                value={fecha}
+                mode="date"
+                display="default"
+                onChange={onChangeDate}
+                 />
+            }
+            
+            <Button  mode="contained" style={styles.containeritem}  onPress={getScheduleFree}>Obtener agenda Libre</Button>
+             <Divider colors={colors.primary}/>
+             <Text style={styles.containeritem}> Crear Reserva:</Text>
+            <DropDown
+                     label={"Cliente"}
+                     mode={"outlined"}
+                     visible={showDropDownClient}
+                     showDropDown={() => setShowDropDownClient(true)}
+                     onDismiss={() => setShowDropDownClient(false)}
+                     value={idCliente}
+                     setValue={setIdCliente}
+                     list={ClientOptions}
+                     style={styles.containeritem}
+            />
+            <Text style={styles.containeritem}> Fecha: {fechaCadena}</Text>
+            <Text style={styles.containeritem}> Hora Inicio: {horaInicioCadena}</Text>
+            <Text style={styles.containeritem}> Hora Fin: {horaFinCadena}</Text>
+            <Button mode="contained" onPress={addReservation}>Crear Reserva</Button>
+            
+        </View>
     )
 }
+const styles=StyleSheet.create({
+    container:{
+        marginHorizontal:10,
+        marginVertical:10
+    },
+    title:{
+        fontSize:15
+    },
+    containeritem:{
+        marginVertical:15
+    }
+})
 export default CrearReserva
--- src/pages/pages/Reserva/EditarReserva.js
+++ src/pages/pages/Reserva/EditarReserva.js
@@ -1,27 +1,79 @@
 import React from "react";
-import { Button, Text } from "react-native-paper";
+import { Button, Text,TextInput } from "react-native-paper";
 import { deleteReservation,updateReservation } from "../../libs/http";
+import { StyleSheet,View } from "react-native";
+import DropDown from "react-native-paper-dropdown";
+import colors from "../../res/colors";
 
 const EditarReserva=()=>{
-   
+   /* -------------States -----------------*/
     const [name,setName]=React.useState('Reserva')
+    const [observacion,setObersvacion]=React.useState('')
+    const [showDropDown, setShowDropDown] = React.useState(false);
+    const [flagAsistio,setFlagAsistio]=React.useState('')
+    const selectOptions=[
+        {
+            'label':'Si',
+            'value':'S'
+        },
+        {
+            'label':'No',
+            'value':'N'
+        }
+    ]
 
+    /* ------------Methods------------- */
     const cancelReservation=async()=>{
         const {data,error}=await deleteReservation(23)
     }
     const updateReservationToServer= async()=>{
+        console.log(observacion,flagAsistio)
         const {data,error}=await updateReservation({
                 idReserva:44,
-                observacion:"hola",
-                flagAsistio:"S"
+                observacion,
+                flagAsistio
         })
     }
+
+    /*-----------UseEffect----------- */
     return(
-        <>
-            <Text> Editar una {name} </Text>
-            <Button onPress={cancelReservation}>Eliminar Reserva</Button>
-            <Button onPress={updateReservationToServer}>Actualizar Reserva </Button>
-        </>
+        <View style={styles.container}> 
+            <TextInput 
+                mode="outlined"
+                label="Obeservacion"
+                value={observacion}
+                onChangeText={text => setObersvacion(text)}
+                style={styles.containeritem}
+            />
+            <DropDown
+                     label={"Asitio"}
+                     mode={"outlined"}
+                     visible={showDropDown}
+                     showDropDown={() => setShowDropDown(true)}
+                     onDismiss={() => setShowDropDown(false)}
+                     value={flagAsistio}
+                     setValue={setFlagAsistio}
+                     list={selectOptions}
+                     style={styles.containeritem}
+            />
+            <View style={[styles.containeritem,{flexDirection:'row',justifyContent:'space-evenly'}]}>
+                <Button mode="contained" color={colors.red} onPress={cancelReservation}>Cancelar </Button>
+                <Button mode="contained" onPress={updateReservationToServer}>Actualizar  </Button>
+            </View>
+            
+        </View>
     )
 }
+const styles=StyleSheet.create({
+    container:{
+        marginHorizontal:10,
+        marginVertical:10
+    },
+    title:{
+        fontSize:15
+    },
+    containeritem:{
+        marginVertical:15
+    }
+})
 export default EditarReserva
